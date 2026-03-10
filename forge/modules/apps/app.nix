934b50d5979f88ed3d005c22e5deb97cdff370ae
eljamm <fedi.jamoussi@protonmail.ch>
2026-03-10 08:13:21 +0100

    refactor: split container config to separate folder

--- forge/modules/apps/app.nix
+++ forge/modules/apps/app.nix
@@ -35,49 +35,12 @@
     };
 
     # Container configuration
-    containers = {
-      enable = lib.mkEnableOption ''
-        Container images output.
-      '';
-      images = lib.mkOption {
-        type = lib.types.listOf (
-          lib.types.submodule {
-            options = {
-              name = lib.mkOption {
-                type = lib.types.str;
-                default = "app-container";
-              };
-              requirements = lib.mkOption {
-                type = lib.types.listOf lib.types.package;
-                default = [ ];
-              };
-              config = {
-                CMD = lib.mkOption {
-                  type = lib.types.listOf lib.types.str;
-                  default = [ ];
-                };
-              };
-            };
-          }
-        );
-        default = [ ];
-        description = "List of container images to build.";
-        example = lib.literalExpression ''
-          [
-            {
-              name = "api";
-              requirements = [ mypkgs.my-package ];
-              config.CMD = [ "my-command" ];
-            }
-          ]
-        '';
+    containers = lib.mkOption {
+      type = lib.types.submodule {
+        imports = [ ./containers ];
       };
-      composeFile = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
-        description = "Relative path to a container compose file.";
-        example = "./compose.yaml";
-      };
+      default = { };
+      description = "Container configuration.";
     };
 
     # Virtual machine
