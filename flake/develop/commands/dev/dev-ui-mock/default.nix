112e14d048c505f64467aaab591b0f4403ebb98c
phanirithvij <phanirithvij2000@gmail.com>
2026-03-31 19:59:32 +0530

    feat(uitests): add script to generate mock backend data
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

---
+++ flake/develop/commands/dev/dev-ui-mock/default.nix
@@ -0,0 +1,18 @@
+# Usage:
+#   nix-shell --run 'dev-ui-mock'
+{
+  replaceVarsWith,
+  runtimeShell,
+}:
+(replaceVarsWith {
+  name = "dev-ui-mock";
+  isExecutable = true;
+  dir = "bin";
+  src = ../dev-ui/ui.sh;
+  replacements = {
+    inherit runtimeShell;
+    defaultListenPort = 3000;
+    mockBackend = "true";
+  };
+  meta.description = "UI dev script which launches with a mock backend";
+})
