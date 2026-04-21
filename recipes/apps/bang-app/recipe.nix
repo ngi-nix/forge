ee7be84a4a1f69af4775d529825711dec2eb7136
Ivan Mincik <ivan.mincik@gmail.com>
2026-04-21 10:54:26 +0200

    recipes: add bang package and app
    Closes https://github.com/ngi-nix/ngipkgs/issues/2254

---
+++ recipes/apps/bang-app/recipe.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  name = "bang-app";
+  displayName = "BANG";
+  description = "Binary Analysis Next Generation framework for recursive unpacking and analysis of binary files.";
+  usage = ''
+    BANG recursively unpacks and classifies binary files, supporting 220+ formats
+    including firmware images, archives, file systems, and executables.
+
+    #### Scan a binary file
+
+    ```
+    bang scan -u /tmp/bang-results /path/to/firmware.bin
+    ```
+
+    _Available in: shell._
+  '';
+
+  links = {
+    source = "https://github.com/armijnhemel/binaryanalysis-ng";
+  };
+
+  ngi.grants = {
+    Review = [
+      "BANG"
+    ];
+  };
+
+  icon = ./icon.svg;
+
+  programs = {
+    packages = [
+      pkgs.mypkgs.bang
+    ];
+    runtimes.shell = {
+      enable = true;
+    };
+  };
+}
