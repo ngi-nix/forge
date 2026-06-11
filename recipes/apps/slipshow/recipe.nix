de07a80c6b1706dce5f164fe7a721f5f3a41d283
Ivan Mincik <ivan.mincik@gmail.com>
2026-06-11 20:39:13 +0200

    recipes: add slipshow app recipe

---
+++ recipes/apps/slipshow/recipe.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  apps.slipshow = {
+    displayName = "Slipshow";
+    description = "Innovative presentation tool that moves away from the traditional slide-based approach.";
+    usage = ''
+      Slipshow is a presentation engine that uses a continuous scrolling canvas
+      instead of discrete slides, allowing for more flexible and dynamic presentations.
+
+      Compile a Slipshow presentation to HTML
+
+      ```bash
+      slipshow compile presentation.md
+      ```
+
+      Start a live preview server
+
+      ```bash
+      slipshow serve presentation.md
+      ```
+    '';
+
+    icon = ./icon.svg;
+
+    links = {
+      website = "https://slipshow.readthedocs.io/en/latest/";
+      source = "https://github.com/panglesd/slipshow";
+    };
+
+    ngi.grants = {
+      Commons = [
+        "Slipshow"
+      ];
+    };
+
+    programs = {
+      packages = [
+        pkgs.slipshow
+      ];
+
+      runtimes.shell = {
+        enable = true;
+      };
+    };
+
+    test.programs.script = ''
+      echo "# Hello" > presentation.md
+      slipshow compile presentation.md
+    '';
+  };
+}
