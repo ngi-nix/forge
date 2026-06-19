3eb2835dc4ef1b4684ac3274c0004cf4fb8fb252
phanirithvij <phanirithvij2000@gmail.com>
2026-06-19 15:28:54 +0530

    WIP don't MERGE
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'path:/shed/Projects/nixhome/nixpkgs/master?lastModified=1781857624&narHash=sha256-QBfJyIjzduyPXDSScBh3SPWT4bN9r7zLEb0IDr0WVv4%3D' (2026-06-19)
      → 'path:/shed/Projects/nixhome/nixpkgs/master_pixelfed-improved?lastModified=1782128450&narHash=sha256-Dl5SqERm%2B8/f5myNdnp26%2BmP63wwmfDAioSxjx1TuHE%3D' (2026-06-22)
    
    flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'path:/shed/Projects/nixhome/nixpkgs/master_pixelfed-improved?lastModified=1782128450&narHash=sha256-Dl5SqERm%2B8/f5myNdnp26%2BmP63wwmfDAioSxjx1TuHE%3D' (2026-06-22)
      → 'github:phanirithvij/nixpkgs/721e2cbf1b1711ace312dc978c873402de94d72c?narHash=sha256-wa2ogRNb7KDOFNTHXy7hbPEXPbZB2AJPnPuW2bghK9A%3D' (2026-06-22)
    
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:phanirithvij/nixpkgs/pixelfed-improved";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
