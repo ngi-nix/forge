66e84feee3f517ecb7431bb935377ad8400f4eed
Julien Moutinho <julm@sourcephile.fr>
2026-06-20 20:34:05 +0200

    +packager@maintainability(forge): init `packages.${package}.phases.unpack.*`

--- forge/modules/pkgs/pkg/phases/unpack.nix
+++ forge/modules/pkgs/pkg/phases/unpack.nix
@@ -1,9 +1,23 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   options = {
+    sourceRoot = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = ''
+        If not empty, path to a subdirectory within the source to use as root of the package's source.
+
+        Use this for monorepos where the source you're interested in is not at the repository root.
+        Format: `"source/<subdir>"`.
+
+        Mapped to `sourceRoot`.
+      '';
+      example = "source/frontend";
+    };
   };
   config = {
     result.derivationAttrs = {
       dontUnpack = !config.enable;
+      sourceRoot = config.sourceRoot;
     };
   };
 }
--- recipes/pkgs/offen-auditorium/recipe.nix
+++ recipes/pkgs/offen-auditorium/recipe.nix
@@ -16,12 +16,12 @@
     build.pnpmPackageBuilder = {
       enable = true;
       pnpmDepsHash = "sha256-xpdFlgHBUcHgL16hruFg6Spv1IlBEc7PB/UqpKnv5Oo=";
-      sourceRoot = "source/auditorium";
       buildScript = "build";
       installDir = "dist";
     };
 
     phases = {
+      unpack.sourceRoot = "source/auditorium";
       build.script.pre = ''
         cp -r ../locales locales
       '';
