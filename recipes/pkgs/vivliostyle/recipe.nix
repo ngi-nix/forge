2a682cb96133b2a3382f59efa9d4c94669cfbebf
eljamm <fedi.jamoussi@protonmail.ch>
2026-07-23 11:03:39 +0200

    recipes(pkgs.vivliostyle): init from nixpkgs derivation

---
+++ recipes/pkgs/vivliostyle/recipe.nix
@@ -0,0 +1,9 @@
+{
+  nixpkgs,
+  ...
+}:
+{
+  # TODO: replace with Nixpkgs derivation when this PR propagates to the forge:
+  # https://github.com/NixOS/nixpkgs/pull/544599
+  pkgs.vivliostyle = nixpkgs.callPackage ./_nixpkgs.nix { };
+}
