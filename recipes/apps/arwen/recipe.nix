783eeb23350b057ee01fd0230cc384a8f76da2d0
Fedi Jamoussi <Fedi.Jamoussi@protonmail.ch>
2026-09-07 14:25:27 +0000

    recipes(apps.arwen): improve usage instructions (#958)

--- recipes/apps/arwen/recipe.nix
+++ recipes/apps/arwen/recipe.nix
@@ -1,7 +1,13 @@
 {
+  config,
   pkgs,
   ...
 }:
+
+let
+  app = config.apps.arwen;
+in
+
 {
   pkgs.arwen = {
     build.identityBuilder = {
@@ -9,77 +15,103 @@
       derivation = pkgs.pkgsOriginal.arwen;
     };
   };
+
   pkgs.python3-py-arwen = {
     build.identityBuilder = {
       enable = true;
       derivation = pkgs.pkgsOriginal.python3.pkgs.py-arwen;
     };
   };
+
   apps.arwen = {
     displayName = "Arwen";
     description = "Cross-platform patching of shared libraries (ELF and Mach-O).";
-    usage = ''
+    longDescription = ''
       Arwen is a command-line utility for patching ELF files (Linux, BSD) and
       Mach-O files (macOS, iOS).
 
-      It is a modern, Rust-based alternative to patchelf and install_name_tool.
+      It's a Rust-based alternative to patchelf and install_name_tool.
+    '';
+    usage = ''
+      First, [launch the shell envrionment](app/${app.name}#run-shell) containing `${app.name}`.
 
-      #### Examples
+      Next, get an executable you want to patch.
+      For the examples below, we will be using the project itself:
 
-      These are a few examples for how to use Arwen.
-      For more, please refer to the [project documentation](https://github.com/nichmor/arwen#usage).
+      ```bash
+      nix build github:ngi-nix/forge#pkgs.arwen
+      cp ./result/bin/arwen ./arwen
+      chmod +w ./arwen
+      ```
 
-      ##### ELF
+      ##### ELF files
 
       Print the rpath:
 
       ```bash
-      arwen elf print-rpath my_binary
+      arwen elf print-rpath ./arwen
       ```
 
       Set the rpath:
 
       ```bash
-      arwen elf set-rpath /path/to/lib my_binary
+      OLD_RPATH="$(arwen elf print-rpath ./arwen)" # saved for later
+      GCC_PATH="$(nix build github:ngi-nix/forge#pkgs.arwen.stdenv.cc.cc.lib --print-out-paths)"
+
+      arwen elf set-rpath "$GCC_PATH/lib" ./arwen
+      arwen elf print-rpath ./arwen
       ```
 
       Remove unused library directories:
 
       ```bash
-      arwen elf shrink-rpath my_binary
+      arwen elf shrink-rpath ./arwen
+      arwen elf print-rpath ./arwen
       ```
 
-      ##### Mach-O
+      Restore the original rpath:
+
+      ```bash
+      arwen elf set-rpath "$OLD_RPATH" ./arwen
+      ```
+
+      ##### Mach-O files
 
       Add an rpath:
 
       ```bash
-      arwen macho add-rpath /path/to/lib my_binary
+      arwen macho add-rpath /path/to/lib ./arwen
       ```
 
       Change an existing rpath:
 
       ```
-      arwen macho change-rpath /old/path /new/path my_binary
+      arwen macho change-rpath /old/path /new/path ./arwen
       ```
 
       Change library install name:
 
       ```bash
-      arwen macho change-install-name /old/libname.dylib /new/libname.dylib my_binary
+      arwen macho change-install-name /old/libname.dylib /new/libname.dylib ./arwen
       ```
+
+      For more details and examples, please see the [project documentation](${config.apps.emerge.links.docs}) page.
     '';
 
     ngi.grants = {
-      Entrust = [ "ELF-rusttools" ];
+      Entrust = [
+        "ELF-rusttools"
+      ];
     };
 
     links = {
       source = "https://github.com/nichmor/arwen";
       website = "https://nichmor.github.io/arwen";
+      docs = "https://github.com/nichmor/arwen#usage";
     };
 
     programs = {
+      mainPackage = pkgs.arwen;
       packages = [
         pkgs.arwen
         (pkgs.python3.withPackages (ps: [
@@ -87,6 +119,7 @@
         ]))
       ];
 
+      runtimes.program.enable = true;
       runtimes.shell.enable = true;
     };
   };
