d94cf59761aa724462b1ff0455cab8d39707eabc
Ivan Mincik <ivan.mincik@gmail.com>
2026-09-10 11:33:48 +0200

    recipes(rpki-client): 9.8 -> 9.9 (#916)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- recipes/pkgs/rpki-client/recipe.nix
+++ recipes/pkgs/rpki-client/recipe.nix
@@ -5,24 +5,27 @@
 
 {
   pkgs.rpki-client = {
-    version = "9.8";
+    version = "9.9";
     description = "Port of OpenBSD's rpki-client RPKI relying party validator to other operating systems.";
     homePage = "https://www.rpki-client.org";
     mainProgram = "rpki-client";
     license = "isc";
 
     source = {
-      git = "github:rpki-client/rpki-client-portable/9.8";
-      hash = "sha256-PejvnEGr+K+g+vLgO+JroZXRAa1LUJUzCwDVm8AyScY=";
+      git = "github:rpki-client/rpki-client-portable/9.9";
+      hash = "sha256-YYEz2F0R15Fr+eYO9RIXW8+BOh3sJA7acmN1d4ka0ns=";
     };
 
     build = {
       extraAttrs = {
+        # IMPORTANT:
+        # rpki-client-openbsd needs to be updated along with rpki-client to the
+        # version matching rpki-client release date.
         openbsdSrc = pkgs.fetchFromGitHub {
           owner = "rpki-client";
           repo = "rpki-client-openbsd";
-          rev = "027566b8e6827a9e280a0ef067464fc2336f0179";
-          hash = "sha256-lmyECC4uhBLJb89Gm+oqO4ClkkhFGqGm+cD7GivDqok=";
+          rev = "65b0882149d7c1a22208b0415b9ece507d8f9522";
+          hash = "sha256-SVAe68oVTuQHcVD+KbbRPm0wNrrV4kc8VG20a1BUZ1w=";
         };
         configureFlags = [
           "--with-base-dir=/var/cache/rpki-client"
