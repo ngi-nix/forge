47e1eb31aa4804a1dbd63515dd1090d18db0297e
eljamm <fedi.jamoussi@protonmail.ch>
2026-09-11 09:32:32 +0200

    progress

--- recipes/apps/zelph/recipe.nix
+++ recipes/apps/zelph/recipe.nix
@@ -41,27 +41,73 @@ in
 
     usage = ''
       You can try out Zelph in your browser without any installation through the [official playground](https://zelph.org/play) page.
+      You can still follow along with the examples we'll be going through, although we will be running our own local playground as well.
 
-      [Core Concepts](https://zelph.org/concepts/#the-self-fact-prefix) page.
+      Before we start, you could have a look at the first few sections of the [Core Concepts](https://zelph.org/concepts/#the-self-fact-prefix) page to better understand the examples.
+      Although this isn't strictly required.
 
-      ##### Command Line
+      You could also check out the [video presentation](https://zelph.org/presentation) which introduces the project and goes through some of its concepts as well.
+
+      ##### Simple Semantic Network
+
+      In this example, we will define relationships between Nix-ecosystem components.
 
       First, [launch the shell environment](app/${app.name}#run-shell) containing `${app.name}`.
 
+      Then, start the `zelph` REPL:
+
       ```bash
-      -- Define Nix ecosystem relationships
-      zelph> Nix "is a" "Package Manager"
-      zelph> Nixpkgs "~" "Repository"
-      zelph> Nixpkgs "contains" Nix
-      zelph> NixOS "~" "Operating System"
-      zelph> NixOS "is built with" Nix
+      $ zelph
+      zelph 1.0.1
+      -- REPL mode - type .help for commands, e.g. '.help .deductions' to change what is shown --
 
-      -- Define a rule: anything built with a package manager "uses" it
-      zelph> (X "is built with" Y, Y "is a" Z) => (X "uses" Z)
-
-      -- Trigger inference
-      zelph> .run
       ```
+
+      Next, let's define what the following terms are:
+
+      ```bash
+      zelph> Nix "is a" "Package Manager"
+      zelph> Nixpkgs ~ "Repository"
+      zelph> NixOS ~ "Operating System"
+      ```
+
+      Note that the `~` symbol is identical in function to `"is a"`.
+      The only difference is that it's one of the [pre-defined core nodes](https://zelph.org/concepts/#predefined-core-nodes) that `zelph` offers, whereas `"is a"` is our [custom one](https://zelph.org/concepts/#working-with-custom-relations).
+
+      Now that we know what things are, we will define some relations between them:
+
+      ```bash
+      zelph> Nixpkgs contains Nix
+      zelph> NixOS "is built with" Nix
+      ```
+
+      > [!NOTE]
+      > When relations contain spaces, they must be enclosed in quotation marks: `"`
+
+      Then, we'll also define a [rule](https://zelph.org/rules/#rules-and-inference) which will be inferred by the `zelph` engine:
+
+      ```bash
+      zelph> (X "is built with" Y, Y "is a" Z) => (X uses Z)
+      ```
+
+      Rules are in the format of: `conditions => consequences`.
+      So in this example, if:
+
+      1. `X "is built with" Y`
+      1. `Y "is a" Z`
+
+      Then `X uses Z`.
+
+      After entering the rule in the REPL, the engine will apply it to nodes that satisfy its conditions and will return the following output:
+
+      ```
+      ((Y "is a" Z), (X "is built with" Y)) => (X uses Z)
+      (NixOS uses "Package Manager") ⇐ {(Nix "is a" "Package Manager") (NixOS "is built with" Nix)}
+      ```
+
+      > [!NOTE]
+      > By default, the inference engine is triggered after every rule or fact is entered, which can be toggled with the `.auto-run` command.
+      > For more information, see the [Performing Inference](https://zelph.org/rules/#performing-inference) page.
 
       ##### Local Playground
     '';
